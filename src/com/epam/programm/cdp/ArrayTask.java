ae3c7f4ba3e64bfc0098a0f7c44ab07302a48603
Ina Nizhnikava <ina_nizhnikava@epam.com>
2016-12-19 16:56:47 +0300

    Commit only

--- src/com/epam/programm/cdp/ArrayTask.java
+++ src/com/epam/programm/cdp/ArrayTask.java
@@ -6,6 +6,7 @@ public class ArrayTask {
 		System.out.println("Hello world");
 		System.out.println("Hello world - GIT");
 		System.out.println("Hello world - GIT - Updated");
+		System.out.println("Hello world - Commit message");
 	}
 
 }
