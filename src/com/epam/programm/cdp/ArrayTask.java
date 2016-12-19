e1ad4cb7bde990c4a35b9175e3804ffc09049cdb
Ina Nizhnikava <ina_nizhnikava@epam.com>
2016-12-19 16:54:45 +0300

    Update ArrayTask - add new output

--- src/com/epam/programm/cdp/ArrayTask.java
+++ src/com/epam/programm/cdp/ArrayTask.java
@@ -5,6 +5,7 @@ public class ArrayTask {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello world - GIT");
+		System.out.println("Hello world - GIT - Updated");
 	}
 
 }
