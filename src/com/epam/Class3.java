c88bff75f73da01f1bd83ed354674699583ecd80
Ina Nizhnikava <ina_nizhnikava@epam.com>
2016-12-19 17:09:14 +0300

    Class2/3 updates

--- src/com/epam/Class3.java
+++ src/com/epam/Class3.java
@@ -4,5 +4,6 @@ public class Class3 {
 
 	public static void main(String[] args) {
 		System.out.println("Class 3 online updates");
+		System.out.println("Class 3 local updates");
 	}
 }
--- src/com/epam/programm/cdp/Class2.java
+++ src/com/epam/programm/cdp/Class2.java
@@ -3,13 +3,7 @@ package com.epam.programm.cdp;
 public class Class2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("Hello world - GIT");
-		System.out.println("Hello world - GIT - Updated");
-		System.out.println("Hello world - Commit message");
-		System.out.println("Hello world - Online message");
-		System.out.println("Hello world - Merge online");
-		System.out.println("Hello world - Merge local");
+		System.out.println("Class 2 local updates");
 		
 		
 		System.out.println("Class 2 online updates");
