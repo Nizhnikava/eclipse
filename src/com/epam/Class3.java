524fcb0d1fa1e0f384925e6beae37ff0238ed2bf
Ina Nizhnikava <ina_nizhnikava@epam.com>
2016-12-19 17:06:58 +0300

    Add new files

---
+++ src/com/epam/Class3.java
@@ -0,0 +1,14 @@
+package com.epam;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world - GIT");
+		System.out.println("Hello world - GIT - Updated");
+		System.out.println("Hello world - Commit message");
+		System.out.println("Hello world - Online message");
+		System.out.println("Hello world - Merge online");
+		System.out.println("Hello world - Merge local");
+	}
+}
